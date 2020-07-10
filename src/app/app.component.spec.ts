a8398dbd3669de1e4c2f43ce47f092dc4880e9c9
Maher <mahernajar87@gmail.com>
2020-07-10 15:19:31 +0200

    Unit test app.component.ts

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BehaviorSubject, of } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { SharedModule } from './components/commons/shared.module';
+describe('AppComponent', () => {
+  const serviceStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    }),
+  };
+
+  const authState: any = {
+    displayName: null,
+    isAnonymous: true,
+    uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2',
+  };
+  const mockAngularFireAuth: any = {
+    auth: jasmine.createSpyObj('auth', {
+      signInAnonymously: Promise.reject({
+        code: 'auth/operation-not-allowed',
+      }),
+      // 'signInWithPopup': Promise.reject(),
+      // 'signOut': Promise.reject()
+    }),
+    authState: of(authState),
+  };
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule, SharedModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: AngularFirestore, useValue: serviceStub },
+        { provide: AngularFireAuth, useValue: mockAngularFireAuth },
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+});
