60adcb97a8308aa0002d198c0f7e2f4f2e2bfa50
Maher Najar <maher.najar@e2vr.io>
2020-12-01 21:56:03 +0100

    update @angular/core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import {
-  AngularFireAnalyticsModule,
-  ScreenTrackingService,
-  UserTrackingService,
-} from '@angular/fire/analytics';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 
 import { environment } from '../environments/environment';
 
@@ -20,26 +14,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './components/commons/shared.module';
 
-registerLocaleData(localeFr, 'fr');
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAnalyticsModule,
-    AngularFirestoreModule,
     AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireAnalyticsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
     SharedModule,
-  ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'fr' },
-    ScreenTrackingService,
-    UserTrackingService,
   ],
   bootstrap: [AppComponent],
 })
