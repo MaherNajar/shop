0ff32736078eec83303729641f032be2c3d7ea2d
Maher Najar <maher.najar@e2vr.io>
2022-10-25 23:53:05 +0200

    locale FR

--- src/app/components/commons/shared.module.ts
+++ src/app/components/commons/shared.module.ts
@@ -1,7 +1,10 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 import { NgbToastModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
