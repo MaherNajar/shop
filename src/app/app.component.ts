0f4aec839a417f8e614b11bc0d87a316ccd7f407
Maher Najar <maher.najar@e2vr.io>
2024-02-14 19:55:53 +0100

    packages update + fix SwUpdate deprecation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 import { LocationService } from './services/location.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,24 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.swUpdate.available.subscribe((event) => {
-      location.reload();
+    const updatesAvailable = this.swUpdate.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      map((evt) => ({
+        type: 'UPDATE_AVAILABLE',
+        current: evt.currentVersion,
+        available: evt.latestVersion,
+      }))
+    );
+
+    updatesAvailable.subscribe({
+      next: (x) => {
+        console.log(`current version: ${x.current}`);
+        console.log(`available version: ${x.available}`);
+        if (x.available > x.current) this.swUpdate.activateUpdate();
+      },
+      error: (e) => console.log(e),
     });
 
-    setInterval(() => {
-      this.swUpdate.checkForUpdate();
-    }, 1000);
     this.locService.getLocation();
     this.cartService.getCart();
   }
