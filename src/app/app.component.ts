bcab40a94f2829b3cc4921d3e41e71f684a601df
Maher Najar <maher.najar@e2vr.io>
2024-03-02 17:42:18 +0100

    SwUpdate + packages update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { SwUpdate } from '@angular/service-worker';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 import { LocationService } from './services/location.service';
-import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -16,23 +15,19 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const updatesAvailable = this.swUpdate.versionUpdates.pipe(
-      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
-      map((evt) => ({
-        type: 'UPDATE_AVAILABLE',
-        current: evt.currentVersion,
-        available: evt.latestVersion,
-      }))
-    );
-
-    updatesAvailable.subscribe({
-      next: (x) => {
-        console.log(`current version: ${x.current}`);
-        console.log(`available version: ${x.available}`);
-        if (x.available > x.current) this.swUpdate.activateUpdate();
-      },
-      error: (e) => console.log(e),
-    });
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          if (
+            confirm(
+              'Nouvelle version disponible. Charger le nouvelle version ?'
+            )
+          ) {
+            window.location.reload();
+          }
+        }
+      });
+    }
 
     this.locService.getLocation();
     this.cartService.getCart();
